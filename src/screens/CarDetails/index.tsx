1faef710d4412f052e3f9fd94a315af8c4a84a38
juliomchado <juliocarlos00@hotmail.com>
2021-10-04 01:29:54 -0300

    add/ details and about

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -6,7 +6,16 @@ import { StatusBar } from 'react-native';
 import {
     Container,
     Header,
-    CarImages
+    CarImages,
+    Content,
+    Details,
+    Description,
+    Brand,
+    Name,
+    Rent,
+    Period,
+    Price,
+    About
 } from './styles';
 
 export function CarDetails() {
@@ -27,6 +36,25 @@ export function CarDetails() {
                     imagesUrl={['https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/vhs/Audi-RS5-Coupe.png']}
                 />
             </CarImages>
+
+            <Content>
+                <Details>
+                    <Description>
+                        <Brand>Lamborghini</Brand>
+                        <Name>Huracan</Name>
+                    </Description>
+
+                    <Rent>
+                        <Period>Ao dia</Period>
+                        <Price>R$ 580</Price>
+                    </Rent>
+                </Details>
+
+                <About>Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na
+                    praça Real Maestranza de Sevilla.
+                    É um belíssimo carro para quem gosta de acelerar.
+                </About>
+            </Content>
         </Container>
     );
 }
