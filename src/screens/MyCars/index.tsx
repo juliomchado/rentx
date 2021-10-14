0e2d45aac1973874a5b914990cb8c3cd42594875
juliomchado <juliocarlos00@hotmail.com>
2021-10-14 20:05:38 +0100

    add/ list my cars

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,22 +1,47 @@
 import React, { useState, useEffect } from 'react';
+import { StatusBar, FlatList } from 'react-native';
+
 import { api } from '../../services/api';
 import { CarDTO } from './../../dtos/CarDTO';
 
 import {
-    Container
+    Container,
+    Header,
+    Title,
+    Subtitle,
+    Content,
+    Appointments,
+    AppointmentsTitle,
+    AppointmentsQuantity,
+
 } from './styles';
+
+import { BackButton } from '../../components/BackButton';
+import { useNavigation } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
+import { CardCar } from '../../components/CardCar';
+import { Load } from '../Load';
+
+interface CarProps {
+    id: string;
+    user_id: string;
+    car: CarDTO;
+};
 
 export function MyCars() {
 
-    const [cars, setCars] = useState<CarDTO[]>([]);
+    const [cars, setCars] = useState<CarProps[]>([]);
     const [loading, setLoading] = useState(true);
+
+    const navigation = useNavigation();
+    const theme = useTheme();
 
     useEffect(() => {
 
         async function fetchCars() {
 
             try {
-                const { data } = await api.get<CarDTO[]>('/schedules_byuser?user_id=1');
+                const { data } = await api.get<CarProps[]>('/schedules_byuser?user_id=1');
 
                 console.log(data)
                 setCars(data);
@@ -30,11 +55,55 @@ export function MyCars() {
 
 
         fetchCars();
-    }, [])
+    }, []);
+
+    function handleBack() {
+        navigation.goBack();
+    };
+
 
     return (
         <Container>
+            <Header>
+                <StatusBar
+                    barStyle="light-content"
+                    translucent
+                    backgroundColor="transparent"
+                />
+                <BackButton
+                    onPress={handleBack}
+                    color={theme.colors.shape}
+                />
 
+                <Title>
+                    Escolha uma {'\n'}
+                    data de início e {'\n'}
+                    fim do aluguel
+                </Title>
+
+                <Subtitle>
+                    Conforto, segurança e praticidade.
+                </Subtitle>
+            </Header>
+            {
+                loading ? <Load /> :
+                    <Content>
+                        <Appointments>
+                            <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+                            <AppointmentsQuantity>{cars.length}</AppointmentsQuantity>
+                        </Appointments>
+
+                        <FlatList
+                            data={cars}
+                            keyExtractor={item => String(item.id)}
+                            showsVerticalScrollIndicator={false}
+                            renderItem={({ item }) => (
+                                <CardCar data={item.car} />
+
+                            )}
+                        />
+                    </Content>
+            }
         </Container>
     );
 }
