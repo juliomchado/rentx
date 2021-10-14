323b018b46d2eba15cd6aa1759e66d657bf1c16b
juliomchado <juliocarlos00@hotmail.com>
2021-10-14 19:29:00 +0100

    add/ scheduling by user

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { api } from '../../services/api';
+import { CarDTO } from './../../dtos/CarDTO';
 
 import {
     Container
 } from './styles';
 
 export function MyCars() {
+
+    const [cars, setCars] = useState<CarDTO[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        async function fetchCars() {
+
+            try {
+                const { data } = await api.get<CarDTO[]>('/schedules_byuser?user_id=1');
+
+                console.log(data)
+                setCars(data);
+
+            } catch (err) {
+                console.log(err);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+
+        fetchCars();
+    }, [])
+
     return (
         <Container>
 
--- src/screens/SchedulingDetails/index.tsx
+++ src/screens/SchedulingDetails/index.tsx
@@ -68,10 +68,16 @@ export function SchedulingDetails() {
 
     async function handleConfirmRental() {
         const schedulesByCar = await api.get(`/schedules_bycars/${car.id}`);
+
         const unavailable_dates = [
             ...schedulesByCar.data.unavailable_dates,
             ...dates,
-        ]
+        ];
+
+        await api.post('schedules_byuser', {
+            user_id: 1,
+            car,
+        });
 
         await api.put(`/schedules_bycars/${car.id}`, {
             id: car.id,
